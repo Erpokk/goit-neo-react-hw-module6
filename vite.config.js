484be98313c8bb65f1422203f0189efeba750826
Maksym Adamenko <maimav56@gmail.com>
2024-12-21 00:23:35 +0200

    redone config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-
 export default defineConfig({
   plugins: [react()],
-  base: '/goit-react-hw-06/',
+  base: "/",
   build: {
     sourcemap: true,
-  }
+  },
 });
